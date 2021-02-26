44361828b6ece8784b59dd370c9853ce05d50d9b
Ian Holladay <holladayian@gmail.com>
2021-02-26 10:55:25 -0700

    add person interface and add to Props interface

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
 interface Props {
   text: string;
+  ok: boolean;
+  i: number;
+  fn: (bob: string) => string;
+  person: Person;
 }
 
 export const TextField: React.FC<Props> = () => {
